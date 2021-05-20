a42a1342a0595c67f25c5c3dfd50b7dd7d01d9d8
tahmidefaz <tahmidefaz@gmail.com>
2021-05-20 11:16:50 -0400

    first pass of options bar

--- src/components/SearchView.js
+++ src/components/SearchView.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import { View } from 'react-native';
+import OptionsBar from './OptionsBar';
 import SearchBar from './SearchBar';
 import SearchList from './SearchList';
+
 
 
 const SearchView = () => {
     return (
         <View>
             <SearchBar/>
+            <OptionsBar/>
             <SearchList/>
         </View>
     )
